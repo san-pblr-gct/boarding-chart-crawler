2668ad7dac52369405203b4d7b0f26966d9667a0
santhosh.kumar <santhosh.jayaraman88@gmail.com>
2018-05-09 10:58:37 +0530

    Added trip date and day

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var fs = require('fs');
 var file = fs.readFileSync('data.json', 'utf8');
 var data = JSON.parse(file);
 var chart = data.templateData.boardingchart;
+var tripDate=data.templateData.DOJ.split(",")[0];
+var tripDay=data.templateData.DOJ.split(",")[1];
 var list = [];
 Object.keys(chart).forEach(key => {
     for (var i = 0; i < chart[key].length; i++) {
-        list.push(chart[key][i])
+        chart[key][i].tripDate=tripDate;
+        chart[key][i].tripDay=tripDay;
+        list.push(chart[key][i]);
     }
 }
 );
